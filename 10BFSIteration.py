f29f9be847f5bc17f354661e2454e36917a5a80e
Itrat Rahman <rahmanitrat@gmail.com>
2016-05-04 20:27:59 +0600

    6th Commit

--- 10BFSIteration.py
+++ 10BFSIteration.py
@@ -5,162 +5,92 @@
 from mrjob.job import MRJob
 from mrjob.protocol import RawValueProtocol
 
-##Class node 
 class Node:
-
-	##Variable initialization function
-	def __init__(self):
-	
-		##A class variable to store the character Id
-		self.characterID = ''
-		##A class variable to store the list of connections
+    def __init__(self):
+        self.characterID = ''
         self.connections = []
-		##A class variable to store the distance of a Node
         self.distance = 9999
-		##A class varaible to store the color of the Node
         self.color = 'WHITE'
-	
-	##A function to format the data extracted from the line
-	#Format is ID|EDGES|DISTANCE|COLOR
-	def fromLine(self, line):
-	
-		##Extracting the pip delimited data from the line
-		fields = line.split('|')
-		
-		##if statement to make sure the length of the field extracted from the line is 4
+
+    #Format is ID|EDGES|DISTANCE|COLOR
+    def fromLine(self, line):
+        fields = line.split('|')
         if (len(fields) == 4):
-		
-			##Extracting the characterID from the line
             self.characterID = fields[0]
-			##Extracting the comma separated data from the line
             self.connections = fields[1].split(',')
-			##Extracting the distance from the line
             self.distance = int(fields[2])
-			##Extracting the color from the line
             self.color = fields[3]
-	
-	
-	##Processing the line to in readable format
-	def getLine(self):
-	
-		#Join all the connections stored in the list as comma separated data
+
+    def getLine(self):
         connections = ','.join(self.connections)
-		
-		##Join the following fields as pip delimited data
-        return '|'.join( (self.characterID, connections, str(self.distance), self.color))
-	
-		
+        return '|'.join( (self.characterID, connections, str(self.distance), self.color) )
 
-##The MapReduce class
-class MRBFSIteration(MRJob):	
+class MRBFSIteration(MRJob):
 
-	##Input & Output Protocol
-	INPUT_PROTOCOL = RawValueProtocol
+    INPUT_PROTOCOL = RawValueProtocol
     OUTPUT_PROTOCOL = RawValueProtocol
 
-	##Setting up configuration
     def configure_options(self):
-        
-		##Setting up configuration
-		super(MRBFSIteration, self).configure_options()
-        
-		#Add pass through option to pass a an argument to the entire MapReduce job
-		self.add_passthrough_option(
+        super(MRBFSIteration, self).configure_options()
+        self.add_passthrough_option(
             '--target', help="ID of character we are searching for")
-	
-	
-	def mapper(self, _, line):
-	
-		##Creating an object of the node class
+
+    def mapper(self, _, line):
         node = Node()
-		
-		##Calling the Node class method fromLine with the line as argument
         node.fromLine(line)
-        
-		#If the node color is GRAY then node needs to be expanded...
+        #If this node needs to be expanded...
         if (node.color == 'GRAY'):
-			
-			##Iterating through every connection in the node connections
             for connection in node.connections:
-			
-				##Passing the object of node by reference
                 vnode = Node()
-				##Setting the character ID of the node
                 vnode.characterID = connection
-				##Updating the distance of the node i.e. adding one to the retrieved distance
                 vnode.distance = int(node.distance) + 1
-                ##Updating the color of the node to GRAY, meaning this node is about to explored
-				vnode.color = 'GRAY'
-				##If the target passed in the command line matches the current character ID under consideration in current iteration
+                vnode.color = 'GRAY'
                 if (self.options.target == connection):
-                    
-					##A varaible to store the useful information of the character id under consideration in a tupl
-					counterName = ("Target ID " + connection +
+                    counterName = ("Target ID " + connection +
                         " was hit with distance " + str(vnode.distance))
-                    
-					##Incrementing the MapReduce class's counter
-					self.increment_counter('Degrees of Separation',
+                    self.increment_counter('Degrees of Separation',
                         counterName, 1)
-						
-				##Yielding the character id and the processed line of data for the character id under consideration in an iterator
                 yield connection, vnode.getLine()
 
-			#We've processed this node, so color it black
+            #We've processed this node, so color it black
             node.color = 'BLACK'
-		
-	##The reducer function of the MapReduce job
-	def reducer(self, key, values):
-			
-			##Default value of edges, distance, and color
-			edges = []
-			distance = 9999
-			color = 'WHITE'
 
-			##The for loop iterates through every line of the value
-			#and updating the processed line of data based on condition
-			for value in values:
-				
-				##Creating an object of the node class
-				node = Node()
-				
-				##Calling the Node class method fromLine with the line as argument
-				node.fromLine(value)
-				
-				##If the number of associated movie character is more than one, 
-				#then the list is initialised to the conncections of the node
-				if (len(node.connections) > 0):
-					#edges = node.connections
-					edges.extend(node.connections)
+        #Emit the input node so we don't lose it.
+        yield node.characterID, node.getLine()
 
-				##If the distance of node of value under consideration is less than the default distance
-				#then the distance is initialised to node distance
-				if (node.distance < distance):
-					distance = node.distance
+    def reducer(self, key, values):
+        edges = []
+        distance = 9999
+        color = 'WHITE'
 
-				##Color is update based on node.color of the value under consideration
-				if ( node.color == 'BLACK' ):
-					color = 'BLACK'
+        for value in values:
+            node = Node()
+            node.fromLine(value)
 
-				if ( node.color == 'GRAY' and color == 'WHITE' ):
-					color = 'GRAY'
+            if (len(node.connections) > 0):
+                #edges = node.connections
+                edges.extend(node.connections)
 
-			##To make sure every parameter of node is updated if they are not updated previously
-			node = Node()
-			node.characterID = key
-			node.distance = distance
-			node.color = color
-			#There's a bug in mrjob for Windows where sorting fails
-			#with too much data. As a workaround, we're limiting the
-			#number of edges to 500 here. You'd remove the [:500] if you
-			#were running this for real on a Linux cluster.
-			node.connections = edges[:500]
-			
-			yield key, node.getLine()
-	
-	
-##Main method of the file
+            if (node.distance < distance):
+                distance = node.distance
+
+            if ( node.color == 'BLACK' ):
+                color = 'BLACK'
+
+            if ( node.color == 'GRAY' and color == 'WHITE' ):
+                color = 'GRAY'
+
+        node = Node()
+        node.characterID = key
+        node.distance = distance
+        node.color = color
+        #There's a bug in mrjob for Windows where sorting fails
+        #with too much data. As a workaround, we're limiting the
+        #number of edges to 500 here. You'd remove the [:500] if you
+        #were running this for real on a Linux cluster.
+        node.connections = edges[:500]
+
+        yield key, node.getLine()
+
 if __name__ == '__main__':
-
-	##Running the map reduce class above
-	MRBFSIteration.run()
-	
+    MRBFSIteration.run()
--- 9ProcessMarvel.py
+++ 9ProcessMarvel.py
@@ -14,55 +14,51 @@ print 'Creating BFS starting input for character ' + sys.argv[1]
 with open("BFS-iteration-0.txt", 'w') as out:
 
 	##Opening the file for reading, extracting the relevant data for processing
-	with open("Marvel-graph.txt") as f:
-	
+    with open("Marvel-graph.txt") as f:
+
 		##Iterating trought every line in the file "Marvel-graph.txt"
-		for line in f:
+        for line in f:
 		
 			##Splitting the space delimited data in the file
-			fields = line.split()
+            fields = line.split()
 			
 			##Extracting the movie character which is the first field in the line
-			heroID = fields[0]
+            heroID = fields[0]
 			
 			##Number of associated connections of the movie character
-			numConnections = len(fields) - 1
+            numConnections = len(fields) - 1
 			
 			##A list of chracter ID of the associated connections
-			connections = fields[-numConnections:]
-			
+            connections = fields[-numConnections:]
+
 			##Default value of color and distance if the heroID 
 			#is not that of movie character under consideration in the current iteration
-			color = 'WHITE'
-			distance = 9999
-			
+            color = 'WHITE'
+            distance = 9999
+
 			##if the heroID matches the movie chracter under consideration in the current iteration
 			#then assign a color of GRAY and a distance of 0
-			if (heroID == sys.argv[1]):
-			
-				color = 'GRAY'
-				
-				distance = 0
-				
+            if (heroID == sys.argv[1]) :
+                color = 'GRAY'
+                distance = 0
+
 			##If statement to check if the heroID is wrongfully extracted as a space character
-			if (heroID != ''):
+            if (heroID != ''):
 			
 				##Join all the connections stored in the list as comma separated data
-				edges = '.'.join(connections)
+                edges = ','.join(connections)
 				
 				##Join the following fields as pip delimited data
-				outStr = '|'.join((heroID, edges, str(distance), color))
-
+                outStr = '|'.join((heroID, edges, str(distance), color))
+				
 				##Writing the string in the file "BFS-iteration-0.txt"
-				out.write(outStr)
+                out.write(outStr)
 				
 				##Writing a new line character at the end of the line
-				out.write("\n")
-	
+                out.write("\n")
+
 	##Closing the file "Marvel-graph.txt"
-	f.close()		
+    f.close()
 	
 ##Closing the file "BFS-iteration-0.txt"
 out.close()
-		
-				
